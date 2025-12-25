b409722afcde051d319a20c5bb86b34607d06178
ahmeddin12 <amudi621@gmail.com>
2025-12-25 11:47:47 +0300

    github authentication complete

---
+++ auth.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+  },
+});
