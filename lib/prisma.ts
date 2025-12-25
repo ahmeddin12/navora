61f0ad8219c6bf3e67a1df6a3f367240b3ffa423
ahmeddin12 <amudi621@gmail.com>
2025-12-25 11:18:06 +0300

    prisma configs

---
+++ lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+// Allow prisma to be undefined initially
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+// Create a new PrismaClient if none exists
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["query"], // optional
+  });
+
+// Prevent multiple instances during dev hot reload
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
